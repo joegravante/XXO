97ccd6644398d2b0bbfc9157f0bcc16486ddd024
Joe <joegravante@gmail.com>
2016-09-25 10:29:17 -0700

    almost there - board update working, winner array is working, - need to finish the game over check using the winner array, check for tie game, add a reset button on Gameover Div, and write the logic for the computa playter

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 var player_turns = 0;
 var computa_turns = 0;
-var player_score = 0;
-var computa_score = 0;
-var winna_winna = [6, 12, 15, 18, 24];
+var player_score = [];
+var computa_score = [];
+var winna_winna = [[1,4,7],[2,5,8],[3,6,9],[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7]];
+var spaces_open = [2,3,5,7,11,13,17,19,23];
 
 function makeaMoveSucka(playerid, space) {
     var token;
@@ -13,31 +14,59 @@ function makeaMoveSucka(playerid, space) {
     }
     document.getElementById(space).innerHTML = token;
     document.getElementById(space).onclick="#";
-
-    countTurns(playerid, space);
+    updateBoard(playerid, space);
+    scoreDaThang(playerid, space);
 }
 
-function countTurns(playerid, space) {
+function updateBoard(playerid, space) {
+    // update the players chosen spaces into the array
+    if (playerid = 1) {
+        player_score.push(space);
+    } else {
+        computa_score.push(space);
+    }
+    // update the spaces left available on the board
+    var index = spaces_open.indexOf(space);
+    spaces_open.splice(index, 1);
+    for (var i = 0; i < winna_winna.length; i++) {
+        console.log (winna_winna[0, i]);
+    }
+}
+
+function scoreDaThang(playerid, space) {
     if (playerid = 1) {
         player_turns ++;
-        player_score += space;
+        //player_score += space;
+        if (doWeHavaWinner(player_score, player_turns) == true) {
+            document.getElementById("gameOver").innerHTML = "yeah, I won!";
+            document.getElementById("gameboard").style.display = "none";
+            document.getElementById("gameOver").style.display = "flex";
+            console.log("we have a winner here, folks");
+        }
+        computasTurn();
     } else {
         computa_turns ++;
-        computa_score += space;
+        //computa_score += space;
+        if (doWeHavaWinner(computa_score, computa_turns) == true) {
+            document.getElementById("gameOver").innerHTML = "crap I lost!";
+            document.getElementById("gameboard").style.display = "none";
+            document.getElementById("gameOver").style.display = "flex";
+        }
     }
 }
 
-function scoreGame(playerid, score, num_turns) {
-    if (num_terns > 2) {
+
+function doWeHavaWinner(score, num_turns) {
+    if (num_turns > 2) {
         for (var i = 0; i < winna_winna.length; i++) {
             if (winna_winna[i] == score) {
-                if (playerid = 1) {
-                    document.getElementById(winner).style.display = block;
-                } else {
-                    document.getElementById(loser).style.display = block;
-                }
+                return true;
             }
-        }
+        } return false;
+    } return false;
+}
 
-    }
+function computasTurn() {
+
+
 }
